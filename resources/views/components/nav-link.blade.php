c241bbc1ca3628000117ab5289c0f089374237f6
=FikriNz <=>
2024-07-24 00:33:03 +0700

    Create Laravel 11

---
+++ resources/views/components/nav-link.blade.php
@@ -0,0 +1,7 @@
+{{-- <a {{ $attributes }}
+    class="{{ request()->is('/') ? 'rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }}"
+    aria-current="{{ request()->is('/') ? 'page' : false }}">{{ $slot }}</a> --}}
+
+<a {{ $attributes }}
+    class="{{ $active ? 'rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }}"
+    aria-current="{{ $active ? 'page' : false }}">{{ $slot }}</a>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <p>Hello World</p>
+</x-layout>
